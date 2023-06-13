ab9baf07813dcb167cca9516b78041f886bc3ea6
Anderson Guassu <andersonguassu@hotmail.com>
2023-06-13 07:19:02 -0300

    preparando para testar as models

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -63,6 +63,15 @@ class RecipeViewsTest(RecipeTestBase):
         # Check if one recipe exist
         self.assertIn(needed_title, content)
 
+    def test_recipe_category_view_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -82,3 +91,13 @@ class RecipeViewsTest(RecipeTestBase):
 
         # Check if one recipe exist
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_view_dont_load_recipe_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:recipe',
+                                           kwargs={'id': recipe.id}
+                                           ))
+        self.assertEqual(response.status_code, 404)
